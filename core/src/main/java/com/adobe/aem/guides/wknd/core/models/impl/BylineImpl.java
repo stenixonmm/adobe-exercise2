0e4d7eb7df2c85d32265fe0d381de84f5abafc13
Remo Liechti <141611642+RemoLiechti@users.noreply.github.com>
2025-02-28 08:45:57 +0100

    change lesson number

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/BylineImpl.java
@@ -79,7 +79,7 @@ public class BylineImpl implements Byline {
     @Override
     public List<String> getOccupations() {
         LOGGER.info("called getOccupations()");
-        occupations = null; // This is specific to L340 RDE lesson 2: remove this line
+        occupations = null; // This is specific to L340 RDE lesson 1: remove this line
         if (occupations != null) {
             LOGGER.debug("Sorting occupations in descending order");
             Collections.sort(occupations);
